1b9a592d34f5e513086c27e824d51eaad4122752
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-09 05:24:29 +0100

    Add license information. Fix copyright.

--- capidup.py
+++ capidup.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python
 
-# Copyright 2010 Israel G. Lugo
-# <israel.lugo@lugosys.com>
-
-# $Id$
+# capidup - quickly find duplicate files in directories
+# Copyright (C) 2010,2014 Israel G. Lugo
+#
+# This file is part of capidup.
+#
+# capidup is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# capidup is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with capidup. If not, see <http://www.gnu.org/licenses/>.
+#
+# For suggestions, feedback or bug reports: israel.lugo@lugosys.com
 
 
 import sys
@@ -267,3 +282,6 @@ if __name__ == '__main__':
     if _errors_while_comparing:
         sys.stderr.write("error: some files could not be compared\n")
         sys.exit(1)
+
+
+# vim: set expandtab smarttab shiftwidth=4 softtabstop=4 tw=75 :
