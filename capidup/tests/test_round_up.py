4f4ebe577cede17f7a3270f376a1967b83ccb077
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-22 03:13:19 +0100

    Use parametrized tests for round_up_to_mult.
    Explicitly declare an array of values and expected results, instead of
    manually running different tests with different data.

--- capidup/tests/test_round_up.py
+++ capidup/tests/test_round_up.py
@@ -23,6 +23,8 @@
 
 import random
 
+import pytest
+
 import capidup.finddups as finddups
 
 
@@ -31,40 +33,31 @@ RANDOM_REPEATS=100
 MAX_RANDOM_NUM=100000000002
 
 
-def try_value(n, mult, expected):
+known_values = [
+    (0, 1000, 0),
+    (100000000002, 100000000001, 200000000002),
+    (100000000001, 100000000002, 100000000002),
+    (1632479, 744829, 2234487),
+]
+
+# generate a list of (n, n, n) tuples, times RANDOM_REPEATS
+random_same = [
+    (_n, _n, _n)
+    for _n in 
+        map(lambda _: random.randrange(0, MAX_RANDOM_NUM),
+            range(RANDOM_REPEATS))
+]
+
+testdata = known_values + random_same
+
+@pytest.mark.parametrize("n, mult, expected", testdata)
+def test_known_values(n, mult, expected):
     """round_up_to_mult(n, m) = e"""
 
     rounded = finddups.round_up_to_mult(n, mult)
 
     assert rounded == expected
 
-def test_zero():
-    """round_up_to_mult(0, x) = 0"""
-
-    try_value(0, 1000, 0)
-
-def test_one_above():
-    """round_up_to_mult(x+1, x) = 2x"""
-
-    try_value(100000000002, 100000000001, 200000000002)
-
-def test_one_below():
-    """round_up_to_mult(x-1, x) = x"""
-
-    try_value(100000000001, 100000000002, 100000000002)
-
-def test_same():
-    """round_up_to_mult(x, x) = x"""
-
-    for _ in range(RANDOM_REPEATS):
-        n = random.randrange(0, MAX_RANDOM_NUM)
-
-        try_value(n, n, n)
-
-def test_primes():
-    """round_up_to_mult(p1, p2) = k*p2"""
-
-    try_value(1632479, 744829, 2234487)
 
 def test_generic():
     """round_up_to_mult(r1, r2) = k*r2"""
