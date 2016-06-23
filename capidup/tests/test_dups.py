694b0eaf0d744266c9981125569e7e94157ac11f
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-23 20:32:41 +0100

    Make test_dups generic. Test dup and non-dup files.
    test_duplicates renamed to test_flat_find_dups_in_dirs. Now takes a
    tuple of tuples: file contents, grouped by those which are equal. This
    lets us give it duplicate files and non-duplicates as well.

--- capidup/tests/test_dups.py
+++ capidup/tests/test_dups.py
@@ -25,11 +25,41 @@ import pytest
 
 import capidup.finddups as finddups
 
-dupsdata = [
-        ("", 3),
-        ("a", 200),
-        ("the quick brown fox jumps over the lazy dog", 2),
-        ("x" * 1000000, 2),
+# Array of data for test_flat_find_dups_in_dirs. Each item is a tuple of
+# tuples: file contents, grouped by those which are equal.
+testdata = [
+    (   # 1 group of 2 duplicate files
+        ("a",) * 2,
+    ),
+    (   # 1 group of 3 empty files (considered duplicates)
+        ("",) * 3,
+    ),
+    (   # 2 groups of dups: 3 empty files, and 200 "a" files
+        ("",) * 3,
+        ("a",) * 200,
+    ),
+    (   # 2 different files, different size
+        ("foo",),
+        ("foobar",),
+    ),
+    (   # 2 different files, same size
+        ("bla",),
+        ("ble",),
+    ),
+    (   # 2 groups of dups and 2 different files
+        ("the quick brown fox jumps over the lazy dog",) * 2,
+        ("The quick brown fox jumps over the lazy dog",) * 2,
+        ("a",),
+        ("b",),
+    ),
+    (   # 2 groups of large files that differ only in the last byte
+        ("x" * 1000000,) * 2,
+        ("x" * 999999 + "X",) * 2,
+    ),
+    (   # 2 groups of large files that differ only in the first byte
+        ("x" * 1000000,) * 2,
+        ("X" + "x" * 999999,) * 2,
+    ),
 ]
 
 
@@ -42,26 +72,47 @@ def test_empty_dir(tmpdir):
     assert(len(dups) == 0)
 
 
-@pytest.mark.parametrize("content, dupcount", dupsdata)
-def test_flat_duplicates(tmpdir, content, dupcount):
-    """Test multiple duplicate files in the same directory.
+@pytest.mark.parametrize("file_groups", testdata)
+def test_flat_find_dups_in_dirs(tmpdir, file_groups):
+    """Test multiple files in the same directory.
 
-    Receives a tmpdir fixture, the content to write in the duplicate files,
-    and the number of duplicate files to create.
+    Receives a tmpdir fixture, and the file content to write in the
+    directory. This should be a tuple of tuples: file contents, grouped by
+    those which are equal. Example:
+
+        (("a", "a"), ("b", "b"), ("c"))
 
     """
-    file_names = []
-    for i in range(dupcount):
-        basename = "file%d" % i
+    name_groups = []
+    group_num = 0
+    for file_group in file_groups:
+        names = []
+        file_num = 0
+        for file_content in file_group:
+            basename = "g%d_file%d" % (group_num, file_num)
 
-        f = tmpdir.join(basename)
-        f.write(content)
+            f = tmpdir.join(basename)
+            f.write(file_content)
 
-        file_names.append(str(f))
+            names.append(str(f))
+            file_num += 1
+
+        name_groups.append(names)
+        group_num += 1
+
+    expected_dups = [names for names in name_groups if len(names) > 1]
 
     dups, errors = finddups.find_duplicates_in_dirs([str(tmpdir)])
 
     assert not errors
-    assert len(dups) == 1
-    assert len(dups[0]) == dupcount
-    assert sorted(dups[0]) == sorted(file_names)
+    assert len(dups) == len(expected_dups)
+
+    # the files may have been traversed in a different order from how we
+    # created them; sort both lists
+    dups.sort()
+    expected_dups.sort()
+
+    for i in range(len(dups)):
+        assert len(dups[i]) == len(expected_dups[i])
+        assert sorted(dups[i]) == sorted(expected_dups[i])
+
