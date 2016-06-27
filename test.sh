193508a8cf58cb557bb0bda95a06655f2ded32a1
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-27 04:10:48 +0100

    Implement test coverage measuring. Report to Codacy.
    Using coverage.py to measure coverage. Using pytest-cov plugin to do it
    automatically while testing.
    
    * .coveragerc: New file, configures coverage.py.
    * test.sh: Configure automated test coverage measuring.
    * .travis.yml: Configure automated coverage measuring in Travis CI.

--- test.sh
+++ test.sh
@@ -21,17 +21,32 @@ unvenv_die() {
 test_python() {
   local python_exe="$1"
   local status
+  local report_to_codacy
+  local codacy_deps
+
+  if [[ -n $CODACY_PROJECT_TOKEN ]]; then
+    report_to_codacy=1
+    codacy_deps="codacy-coverage"
+  else
+    report_to_codacy=0
+    codacy_deps=""
+  fi
 
   venv=$(mktemp -d --tmpdir capidup-venv-XXXXXX) || die "unable to create temp directory"
 
   virtualenv --python="$python_exe" "$venv"
   source "$venv/bin/activate"
 
-  for i in pytest .; do
+  for i in pytest coverage pytest-cov $codacy_deps .; do
     pip install "$i" || unvenv_die "unable to install $i inside virtualenv"
   done
 
-  py.test capidup
+  py.test --cov=capidup capidup
+
+  if (( report_to_codacy )); then
+    coverage xml
+    python-codacy-coverage -r coverage.xml
+  fi
   status=$?
 
   deactivate
@@ -40,6 +55,12 @@ test_python() {
 
   return $status
 }
+
+if (( $# >= 1 )); then
+  # load external script; should define CODACY_PROJECT_TOKEN for test coverage
+  # reporting
+  source "$1"
+fi
 
 test_python "python2"
 test_python "python3"
