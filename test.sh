2db0e35f9a6a2826bf8931636461d61b270a77e2
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-28 18:38:10 +0100

    test.sh: Work when system's python3 is Python 3.2.
    The coverage package, which is used by pytest-cov, doesn't support
    Python 3.2 after version 4.0.
    
    See travis-ci/travis-ci#4866 for more details.

--- test.sh
+++ test.sh
@@ -11,6 +11,17 @@ unvenv_die() {
   die "$1"
 }
 
+
+# Get Python major and minor version.
+#
+# Optionally receives the python executable (defaults to "python").
+#
+get_python_ver() {
+  local pyexe="${1:-python}"
+
+  $pyexe -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))'
+}
+
 # Test a specific Python version.
 #
 # Receives the name of the desired python executable file.
@@ -23,6 +34,7 @@ test_python() {
   local status
   local report_to_codacy
   local codacy_deps
+  local coverage
 
   if [[ -n $CODACY_PROJECT_TOKEN ]]; then
     report_to_codacy=1
@@ -37,7 +49,14 @@ test_python() {
   virtualenv --python="$python_exe" "$venv"
   source "$venv/bin/activate"
 
-  for i in pytest coverage pytest-cov $codacy_deps .; do
+  if [[ $(get_python_ver) == "3.2" ]]; then
+    # Coverage 4.0 dropped support for Python 3.2 (see travis-ci/travis-ci#4866)
+    coverage="coverage<4.0.0.a0"
+  else
+    coverage="coverage"
+  fi
+
+  for i in pytest $coverage pytest-cov $codacy_deps .; do
     pip install "$i" || unvenv_die "unable to install $i inside virtualenv"
   done
 
