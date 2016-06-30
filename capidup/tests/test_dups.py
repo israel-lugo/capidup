66c5109a0ef5bc381cf5c96b7aa1206e89665c27
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-30 21:59:07 +0100

    Test error handling and error strings. See #11.
    * test_dups.py (test_flat_find_dups_in_dirs): New parameters
    num_index_errors and num_read_errors. Function called with all
    combinations of file contents and several values for unreadable
    dirs/files. Refactor file content creation to a separate function, as
    well as unreadable directories and files.  (setup_flat_files,
    setup_flat_dir_errors, setup_flat_read_errors): New functions.

--- capidup/tests/test_dups.py
+++ capidup/tests/test_dups.py
@@ -21,6 +21,7 @@
 
 """Black box duplicates testing."""
 
+import os
 import pytest
 
 import capidup.finddups as finddups
@@ -62,6 +63,13 @@ testdata = [
     ),
 ]
 
+index_errors_data = range(4)
+# Can't include the "1 read error" case because the file may not even be
+# hashed if its size doesn't match anything else's. That's actually a nice
+# test: create an unreadable file with unique size, and make sure no read
+# error occurs (i.e. it wasn't hashed).
+read_errors_data = [0, 2, 3, 4]
+
 
 def test_empty_dir(tmpdir):
     """Test that empty dirs work and produce no duplicates."""
@@ -72,15 +80,13 @@ def test_empty_dir(tmpdir):
     assert len(dups) == 0
 
 
-@pytest.mark.parametrize("file_groups", testdata)
-def test_flat_find_dups_in_dirs(tmpdir, file_groups):
-    """Test multiple files in the same directory.
+def setup_flat_files(tmpdir, file_groups):
+    """Create a flat file structure for testing.
 
     Receives a tmpdir fixture, and the file content to write in the
-    directory. This should be a tuple of tuples: file contents, grouped by
-    those which are equal. Example:
+    directory. See test_flat_find_dups_in_dirs doc for details.
 
-        (("a", "a"), ("b", "b"), ("c"))
+    Returns a list of groups of filenames, equal amongst themselves.
 
     """
     name_groups = []
@@ -98,21 +104,104 @@ def test_flat_find_dups_in_dirs(tmpdir, file_groups):
 
         name_groups.append(names)
 
-    # don't consider groups that only have one file (it will not be
-    # considered a duplicate if it's alone)
+    return name_groups
+
+
+def setup_flat_dir_errors(tmpdir, count):
+    """Create unreadable directories for testing.
+
+    Receives a tmpdir fixture, and the number of unreadable directories to
+    create inside it.
+
+    Doesn't work in Windows: can't portably remove read permissions there.
+
+    Returns a list of pathnames of the directories.
+
+    """
+    dir_errors = []
+    for i in range(count):
+        # create a subdirectory with no permissions
+        basename = "noidx%d" % i
+
+        d = tmpdir.mkdir(basename)
+        os.chmod(str(d), 0)
+
+        dir_errors.append(str(d))
+
+    return dir_errors
+
+
+def setup_flat_read_errors(tmpdir, count):
+    """Create unreadable files for testing.
+
+    Receives a tmpdir fixture, and the number of unreadable files to create
+    inside it. The number should not be 1, as capidup may not even try to
+    hash the file if there's no other file with the same size. That means
+    there would be no error.
+
+    Doesn't work in Windows: can't portably remove read permissions there.
+
+    Returns a list of filenames.
+
+    """
+    read_errors = []
+    for i in range(count):
+        # create a file with no read permission
+        basename = "noread%d" % i
+
+        f = tmpdir.join(basename)
+        # all error files should be the same size, to make sure they are
+        # all actually hashed (if they're different sizes, they won't be)
+        f.write("this will never be read")
+        os.chmod(str(f), 0)
+
+        read_errors.append(str(f))
+
+    return read_errors
+
+
+@pytest.mark.parametrize("file_groups", testdata)
+@pytest.mark.parametrize("num_index_errors", index_errors_data)
+@pytest.mark.parametrize("num_read_errors", read_errors_data)
+def test_flat_find_dups_in_dirs(tmpdir, file_groups, num_index_errors,
+                                num_read_errors):
+    """Test multiple files in the same directory.
+
+    Receives a tmpdir fixture, the file content to write in the directory,
+    the number of unreadable directories to create, and the number of
+    unreadable files to create.
+
+    The file content should be a tuple of tuples: file contents, grouped by
+    those which are equal. Example:
+
+        (("a", "a"), ("b", "b"), ("c"))
+
+    """
+    # create file contents for duplicate testing
+    name_groups = setup_flat_files(tmpdir, file_groups)
+    # discard groups with only one file (unique files are not duplicates)
     expected_dup_groups = [names for names in name_groups if len(names) > 1]
 
+    # create unreadable dirs and files
+    index_errors = setup_flat_dir_errors(tmpdir, num_index_errors)
+    read_errors = setup_flat_read_errors(tmpdir, num_read_errors)
+    expected_errors = index_errors + read_errors
+
+    # run the duplicate search
     dup_groups, errors = finddups.find_duplicates_in_dirs([str(tmpdir)])
 
-    assert not errors
+    assert len(errors) == len(expected_errors)
     assert len(dup_groups) == len(expected_dup_groups)
 
-    # the files may have been traversed in a different order from how we
-    # created them; sort both lists
+    # Check that duplicate groups match. The files may have been traversed
+    # in a different order from how we created them; sort both lists.
     dup_groups.sort()
     expected_dup_groups.sort()
-
     for i in range(len(dup_groups)):
         assert len(dup_groups[i]) == len(expected_dup_groups[i])
         assert sorted(dup_groups[i]) == sorted(expected_dup_groups[i])
 
+    # check that all unreadable files/dirs are mentioned in the errors
+    error_lines = '\n'.join(errors)
+    for error_name in expected_errors:
+        assert error_name in error_lines
