33d6acfb0bd07fa7360fcb32a30753456887c06d
Israel G. Lugo <israel.lugo@lugosys.com>
2016-07-12 01:33:10 +0100

    setup.py: Add metadata (url and classifiers).

--- setup.py
+++ setup.py
@@ -10,9 +10,26 @@ setup(
     description='Quickly find duplicate files in directories',
     author="Israel G. Lugo",
     author_email='israel.lugo@lugosys.com',
+    url='https://github.com/israel-lugo/capidup',
     version=__version__,
     packages=['capidup',],
     license='License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+    ],
     long_description="""
 CapiDup recursively crawls through all the files in a list of directories and
 identifies duplicate files. Duplicate files are files with the exact same
