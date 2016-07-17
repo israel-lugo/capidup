b96185cecb9b7045c01ce60d5253924659facd09
Israel G. Lugo <israel.lugo@lugosys.com>
2016-07-17 17:21:08 +0100

    Remove 'build tools' topic from PyPi classifiers.

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
